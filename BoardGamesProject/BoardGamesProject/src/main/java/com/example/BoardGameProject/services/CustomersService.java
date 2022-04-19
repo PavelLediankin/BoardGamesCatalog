1d399dfa32bbaaed3b58ad906c9e8a4229fb6bf5
Pavel_Lediankin <a.lediankin@yandex.ru>
2022-04-19 15:01:55 +0500

    add CustomersService

---
+++ BoardGamesProject/BoardGamesProject/src/main/java/com/example/BoardGameProject/services/CustomersService.java
@@ -0,0 +1,50 @@
+package com.example.BoardGameProject.services;
+
+import com.example.BoardGameProject.models.Customer;
+import com.example.BoardGameProject.repositories.CustomersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomersService
+{
+    @Autowired
+    CustomersRepository customersRepository;
+
+    @Autowired
+    GameStoreService gameStoreService;
+
+    public void addCustomer(String firstName, String lastName, String address)
+    {
+        var customer = new Customer(firstName, lastName, address);
+        customersRepository.save(customer);
+    }
+
+    public void editCustomer(long id, Customer customer)
+    {
+        var oldCustomer = customersRepository.getById(id);
+        if(customer.getAddress() != null)
+        {
+            oldCustomer.setAddress(customer.getAddress());
+            customersRepository.save(oldCustomer);
+        }
+    }
+
+    public Customer getCustomer(long id)
+    {
+        return customersRepository.getById(id);
+    }
+
+    public void removeCustomer(long id)
+    {
+        customersRepository.deleteById(id);
+    }
+
+    public String getGame(long gameId, long storeId) throws Exception
+    {
+        //TODO Buy(billing) or quasi-buying
+
+        gameStoreService.removeGamesInStore(gameId, storeId, 1);
+        return "OK";
+    }
+}
